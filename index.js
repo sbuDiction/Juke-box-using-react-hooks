a53658cfd815bbf83266421151c75c475158383f
sbuDiction <nsbu68501@gmail.com>
2022-05-19 17:54:55 +0200

    serving build to express server

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const app = express();
 const cors = require('cors');
 const router = require('./server/src/routes/v1.router');
 
-
+app.use(express.static('client/build'));
 app.use(router);
 app.use(cors);
-app.use(express.static('client/build'));
 
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
@@ -18,6 +17,9 @@ app.use((req, res) => {
     res.status(404);
     res.send({ error: "Sorry, can't find that" });
 });
+
+
+
 
 const listener = app.listen(process.env.PORT || 5000, () => {
     console.log("Your app is listening on port " + listener.address().port);
